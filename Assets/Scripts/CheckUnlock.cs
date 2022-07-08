a77c2288640d0a65116045e9a65d1a21d21cc8e6
Esfekia <sertac@gmail.com>
2022-07-08 14:37:01 -0400

    Added CheckUnlock.cs to manage key triggering door to open. added AttachmentPoint to key to ensure it is picked up correctly. Added Key1 tag and now everything works!
    Added CheckUnlock.cs to manage key triggering door to open. added AttachmentPoint to key to ensure it is picked up correctly. Added Key1 tag and now everything works!

---
+++ Assets/Scripts/CheckUnlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckUnlock : MonoBehaviour
+{
+    public GameObject door1 = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        door1.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Key1"))
+        {
+            if (door1.activeInHierarchy)
+            {
+                door1.SetActive(false);
+            }
+            
+        }
+    }
+}
